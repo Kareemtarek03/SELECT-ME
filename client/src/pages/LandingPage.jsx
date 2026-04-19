7e47ab9a35d065ce66cf1966ddd8295b16319daa
Kareem Tarek <kareemtarek03@gmail.com>
2026-04-19 09:35:05 +0200

    release 2.4.0

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -91,7 +91,7 @@ export default function LandingPage() {
                         color="#888"
                         textAlign="center"
                     >
-                        Version 2.3.8
+                        Version 2.4.0
                     </Text>
                 </VStack>
             </Box>
